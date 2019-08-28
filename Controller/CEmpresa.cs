79cdb3c0eadc546bfc53ba86966cd552fa69826e
Rafael Megda <rafaelmisidoro@gmail.com>
2019-08-28 09:41:38 -0300

    Upload Controller

---
+++ Controller/CEmpresa.cs
@@ -0,0 +1,125 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public static class CEmpresa 
+    {
+        //INSERIR
+        public static void Inserir(MEmpresa item)
+        {
+            if (item == null)
+            {
+                throw new Exception("Empresa digitada esta inválida");
+            }
+
+            //NÃO SALVAR CAMPO EM BRANCO OU COM MAIS DE 50 CHARS
+            if (item.NomeEmpresa.Trim() == "" || item.NomeEmpresa.Length > 50)
+            {
+                throw new Exception("O Nome da empresa não pode esta vazia ou conter mais de 50 caracteres");
+            }
+
+            //NÃO PERMITIR SALVAR SOMENTE NUMEROS
+            Regex validarNumero = new Regex (@"^\d+$");
+            if (validarNumero.IsMatch(item.NomeEmpresa))
+            {
+                throw new Exception("Nome da Empresa não pode conter somente numeros");
+            }
+
+            try
+            {
+                DEmpresa.Inserir(item);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+        //PESQUISAR
+        public static List<MEmpresa> Pesquisar(MEmpresa item)
+        {
+            List<MEmpresa> retorno = null;
+
+            if (item.NomeEmpresa != null && item.NomeEmpresa.Length <= 50)
+            {
+                retorno = DEmpresa.Pesquisar(item);
+
+                //RETORNO SE NÃO HOUVER NENHUM CADASTRO
+                if (retorno == null)
+                {
+                    throw new Exception("A Pesquisa não retornou nenhum cadastro!");
+                }
+            }
+
+            return retorno;
+        }
+
+        //EXCLUIR
+        public static void Excluir(MEmpresa item)
+        {
+            if (item.NomeEmpresa.Trim() == "")
+                throw new Exception("Nome da Empresa Inválida");
+
+            try
+            {
+                DEmpresa.Excluir(item);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //OBTER
+        public static MEmpresa Obter(MEmpresa item)
+        {
+            MEmpresa retorno = null;
+
+            if (item != null)
+            {
+                retorno = DEmpresa.Obter(item);
+            }
+
+            return retorno;
+        }
+
+        //ATUALIZAR
+        public static void Atualizar(MEmpresa item)
+        {
+            if (item == null)
+            {
+                throw new Exception("Empresa digitada esta inválida");
+            }
+
+            //NÃO SALVAR CAMPO EM BRANCO OU COM MAIS DE 50 CHARS
+            if (item.NomeEmpresa.Trim() == "" || item.NomeEmpresa.Length > 50)
+            {
+                throw new Exception("O Nome da empresa não pode esta vazia ou conter mais de 50 caracteres");
+            }
+
+            //NÃO SALVAR NUMEROS
+            Regex validarNumero = new Regex(@"^\d+$");
+            if (validarNumero.IsMatch(item.NomeEmpresa))
+            {
+                throw new Exception("Nome da Empresa não pode conter somente numeros");
+            }
+
+            try
+            {
+                DEmpresa.Atualizar(item);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ Controller/CTaxaEmbarque.cs
@@ -0,0 +1,158 @@
+﻿using DAL;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Controller
+{
+    public static class CTaxaEmbarque
+    {
+        //INSERIR --- CONCLUIR VERIFICAÇÕES
+        public static void Inserir(MTaxaEmbarque item)
+        {
+            //REGEX PARA VALIDAR NUMEROS
+            Regex validarNumero = new Regex(@"^\d+$");
+            //Regex validarData = new Regex(@"(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/((1[2-9]|[2-9][0-9])[0-9]{2})");
+            //Regex validarData = new Regex(@"(\d{2}\/\d{2}\/\d{4} \d{2}:\d{2})");
+
+            if (item == null)
+            {
+                throw new Exception("Taxa de Embarque esta incorreta");
+            }
+
+            if (item.EmpresaID == 0)
+            {
+                throw new Exception("É necessário selecionar uma empresa");
+            }
+
+            if(item.Data == "00/00/0000" || item.Data == "/  /") 
+            {
+                throw new Exception("A data esta incorreta");
+            }
+
+            if(item.Quantidade <= 0)
+            {
+                throw new Exception("Campo Quantidade não pode ser negativo ou igual a zero");
+            }
+
+            if(item.ValorTarifa <= 0)
+            {
+                throw new Exception("Campo Valor Tarifa não pode ser negativo ou igual a zero");
+            }
+
+            //VALIDAR DATA
+            //if (validarData.IsMatch(item.Data))
+            //{
+            //    throw new Exception("A data esta incorreta");
+            //}
+
+            //corrigir aqui - esta invalidando tudo - alterar o regex
+            //if (validarData.IsMatch(item.Data))
+            //{
+            //    throw new Exception("A data esta incorreta");
+            //}
+
+            try
+            {
+                DTaxaEmbarque.Inserir(item);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //PESQUISAR
+        public static List<MTaxaEmbarque> Pesquisar(MTaxaEmbarque item)
+        {
+            List<MTaxaEmbarque> retorno = null;
+
+            if (item.id == 0)
+            {
+                retorno = DTaxaEmbarque.Pesquisar(item);
+
+                //RETORNO SE NÃO HOUVER NENHUM CADASTRO
+                if (retorno == null)
+                {
+                    throw new Exception("A Pesquisa não retornou nenhum cadastro!");
+                }
+
+
+            }
+            return retorno;
+        }
+
+        //EXCLUIR
+        public static void Excluir(MTaxaEmbarque item)
+        {
+            if (item.id == 0)
+                throw new Exception("Nome da Empresa Inválida");
+
+            try
+            {
+                DTaxaEmbarque.Excluir(item);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        //OBTER
+        public static MTaxaEmbarque Obter(MTaxaEmbarque item)
+        {
+            MTaxaEmbarque retorno = null;
+
+            if (item != null)
+            {
+                retorno = DTaxaEmbarque.Obter(item);
+            }
+
+            return retorno;
+        }
+
+        //ATUALIZAR
+        public static void Atualizar(MTaxaEmbarque item)
+        {
+            if (item == null)
+            {
+                throw new Exception("Objeto PESSOA inválido");
+            }
+
+            if (item.EmpresaID == 0)
+            {
+                throw new Exception("É necessário selecionar uma empresa");
+            }
+
+            if (item.Data == "00/00/0000" || item.Data == "/  /")
+            {
+                throw new Exception("A data esta incorreta");
+            }
+
+            if (item.Quantidade <= 0)
+            {
+                throw new Exception("Campo Quantidade não pode ser negativo ou igual a zero");
+            }
+
+            if (item.ValorTarifa <= 0)
+            {
+                throw new Exception("Campo Valor Tarifa não pode ser negativo ou igual a zero");
+            }
+
+            try
+            {
+                DTaxaEmbarque.Atualizar(item);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+    }
+}
