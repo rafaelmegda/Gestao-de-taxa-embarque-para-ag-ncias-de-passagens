807d902bec26f774c25c1fe23b1fdf20bb07e0f9
Rafael Megda <rafaelmisidoro@gmail.com>
2019-08-28 09:42:40 -0300

    upload Model

---
+++ Model/MEmpresa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Serializable]
+
+    public class MEmpresa
+    {
+        public int id { get; set; }
+        public string NomeEmpresa { get; set; }
+        
+    }
+}
---
+++ Model/MTaxaEmbarque.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    [Serializable]
+    public class MTaxaEmbarque
+    {
+        public int id { get; set; }
+        public string Data { get; set; }
+        public int Quantidade { get; set; }
+        public decimal ValorTarifa { get; set; }
+        public decimal ValorTotal { get; set; }
+        public int? EmpresaID { get; set; }
+        public string NomeDaEmpresa { get; set; }
+    }
+}
